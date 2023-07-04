788e37d9959e9215bacd97447a8a74a7d3b6f0b9
juanjocpalencia <juanjocpalencia@gmail.com>
2023-07-04 11:34:51 -0600

    updating social media

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -9,10 +9,18 @@ import {
 function SocialMedia() {
   return (
     <div className='Social-Container'>
-        <FacebookFilled className='Social' />
-        <InstagramOutlined className='Social'/>
-        <PhoneOutlined className='Social'/>
-        <WhatsAppOutlined className='Social'/>
+        <a href="https://www.facebook.com/FusionJAMMx/" target='_blank'>
+          <FacebookFilled className='Social Facebook' />
+        </a>
+        <a href="https://www.instagram.com/FusionJAMMx/" target='_blank'>
+          <InstagramOutlined className='Social Instagram'/>
+        </a>
+        <a href="tel:+524442914186">
+          <PhoneOutlined className='Social Phone'/>
+        </a>
+        <a href="https://api.whatsapp.com/send?phone=524442914186&text=Hola me gustaría hacer una cotización" target='_blank'>
+          <WhatsAppOutlined className='Social Whatsapp'/>
+        </a>
     </div>
   )
 }
